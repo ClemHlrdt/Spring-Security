3a4e673c11ad1c3cd2ee4a233c1360fad79903e3
Clément Hélardot <clement.helardot@gmail.com>
2020-05-29 15:54:03 +0200

    - Implemented UserDetailsService with InMemoryUserDetailsManager

--- src/main/java/com/clemhlrdt/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/clemhlrdt/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.clemhlrdt.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -21,4 +26,20 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.httpBasic();
 	}
+
+	// Where we retrieve users from db
+	@Override
+	@Bean // to be instanciated
+	protected UserDetailsService userDetailsService() {
+		UserDetails annaSmithUser = User.builder()
+				.username("annasmith")
+				.password("password")
+				.roles("STUDENT") // ROLE_STUDENT
+				.build();
+
+		return new InMemoryUserDetailsManager(
+				annaSmithUser
+		);
+	}
 }
+
