0b620f341887ffa084d95acace56eb5555ebe89a
Clément Hélardot <clement.helardot@gmail.com>
2020-06-01 17:26:25 +0200

    - Changed default token validity to 21 days & hashed it with a custom value

--- src/main/java/com/clemhlrdt/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/clemhlrdt/springsecurity/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.clemhlrdt.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -44,7 +46,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginPage("/login").permitAll()
 				.defaultSuccessUrl("/courses", true)
 				.and()
-				.rememberMe();	// defaults to 2 weeks
+				.rememberMe()
+					.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+					.key("somethingverysecured");	// defaults to 2 weeks
 	}
 
 	// Where we retrieve users from db
